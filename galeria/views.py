2e55a124a58a4216e03f73f9ff165971fc937ca7
moraismariana <moraismariana200@gmail.com>
2024-02-28 10:11:17 -0300

    Banco de Dados e Admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def pesquisa(request):
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+
+    if 'input' in request.GET:
+        input_da_pesquisa = request.GET['input']
+        if input_da_pesquisa:
+            fotografias = fotografias.filter(nome__icontains=input_da_pesquisa)
+
+    return render(request, 'galeria/pesquisa.html', {'cards': fotografias})
